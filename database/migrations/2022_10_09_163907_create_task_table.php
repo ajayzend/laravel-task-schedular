12a13b4702c868d243fced1e2f1f73baefbb0465
Ajay Sahu <ajay.kumar.iimt@gmail.com>
2022-10-11 09:29:37 +0530

    First Commit

---
+++ database/migrations/2022_10_09_163907_create_task_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use phpDocumentor\Reflection\Types\Null_;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->string('command');
+            $table->string('expression')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->boolean('dont_overlap')->default(false);
+            $table->boolean('run_in_maintenance')->default(false);
+            $table->string('notification_email')->nullable();            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
